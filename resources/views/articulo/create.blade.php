4337c60c05c6bb98df453b24748785d7172acd3e
Ricardo Soto <menjivarsoto09@gmail.com>
2022-04-09 19:30:19 -0600

    crud laravel

---
+++ resources/views/articulo/create.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.plantillabase');
+
+@section('contenido')
+
+<h2>Crear Registros</h2>
+
+<form action="/articulos" method="POST">
+@csrf
+<div class="mb-3">
+    <label for="" class="form-label">Codigo</label>
+    <input type="text" id="codigo" name="codigo" type="text" class="form-control" tabindex="1">
+</div>
+<div class="mb-3">
+    <label for="" class="form-label">Descripción</label>
+    <input id="descripcion" name="descripcion" type="text" class="form-control" tabindex="2">
+</div>
+<div class="mb-3">
+    <label for="" class="form-label">Cantidad</label>
+    <input id="cantidad" name="cantidad" type="number" class="form-control" tabindex="3">
+</div>
+<div class="mb-3">
+    <label for="" class="form-label">Precio</label>
+    <input id="precio" name="precio" type="number" class="form-control" step="any" value="0.00">
+</div>
+<a href="/articulos"  class="btn btn-secondary" tabindex="5">Cancelar</a>
+<button type="submit" class="btn btn-primary" tabindex="4">Guardar</button>
+</form>
+
+@endsection
